7c88099db19a03c8f7e82d2b7e553f5e00ddb5cc
ckoca5209-create <ckoca5209@gmail.com>
2025-12-24 19:18:56 +0300

    Create parola.py

---
+++ parola.py
@@ -0,0 +1,13 @@
+import random
+
+chars = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+password_length = int(input("şifrenin uzunluğunu girin:"))
+
+password = ""
+
+for i in range(password_length):
+    char = random.choice(chars)
+    password += char
+    
+print(password)
